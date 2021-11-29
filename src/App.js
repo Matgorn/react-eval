8fa5e0ac33e90dacda38352a576f9d43204c9168
Matgorn <m.gorny@selleo.com>
2021-11-29 16:40:01 +0100

    temp save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import { About, Home, Users, User } from './screens';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+
+          <Route path="/users" element={<Users />}>
+            <Route path=":userId" element={<User />} />
+          </Route>
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+import About from './About';
+import Home from './Home';
+import Users from './Users';
+import User from './Users/screens/User'
+
+export {
+  About,
+  Home,
+  Users,
+  User
+};
